1de2a1df2cbe616b7c3bfb06a67c65c9f2715fae
alex <alex@gmail.com>
2025-09-14 07:59:54 -0700

    fix admin in accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import (
+    CustomUser,PasswordResetToken
+)
 
-# Register your models here.
+@admin.register(CustomUser)
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username",'email')}),
+        ("Personal info", {"fields": ("first_name", "last_name", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "usable_password", "password1", "password2"),
+            },
+        ),
+    )
+
+@admin.register(PasswordResetToken)
+class PasswordResetTokenAdmin(admin.ModelAdmin):
+    list_display = ['user','token','created_at']
