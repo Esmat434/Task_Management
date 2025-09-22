48d0f6ddd7a6174f79fb204cee3dffdda94a43b6
alex <alex@gmail.com>
2025-09-22 07:20:43 -0700

    add models admin in tasks app

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import (
+    Category,Board,Task
+)
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','user','title','created_at']
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ['id','user','title','category','created_at']
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id','board','title','status','published_at','created_at']
