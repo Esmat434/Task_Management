4d9d71493c57d112c8f214e2e4bc315f70ea2a13
alex <alex@gmail.com>
2025-09-27 07:58:05 -0700

    fix urls in tasks app

---
+++ tasks/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    CategoryCreateListView,CategoryDetailUpdateDestroyView,BoardCreateListView,BoardDetailUpdateDestroyView,
+    TaskCreateListView,TaskDisableListView,TaskDisableDetailView,TaskDetailUpdateDestroyView
+)
+
+app_name = "tasks"
+
+urlpatterns = [
+    path('categories/', CategoryCreateListView.as_view(), name='categories'),
+    path('categories/<int:pk>/', CategoryDetailUpdateDestroyView.as_view(), name='category-detail'),
+    path('boards/', BoardCreateListView.as_view(), name='boards'),
+    path('boards/<int:pk>/', BoardDetailUpdateDestroyView.as_view(), name='board-detail'),
+    path('tasks/', TaskCreateListView.as_view(), name='tasks'),
+    path('task_disable/', TaskDisableListView.as_view(), name="task-disable-list"),
+    path("task_disable/<int:pk>/", TaskDisableDetailView.as_view(), name="task-disable-detail"),
+    path('tasks/<int:pk>/', TaskDetailUpdateDestroyView.as_view(), name='task-detail')
+]
