eb71d494a5a63763771b7dbd65463626c42bbfac
alex <alex@gmail.com>
2025-10-08 23:47:04 -0700

    fix settings of projects

--- TaskManagement/settings.py
+++ TaskManagement/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','127.0.0.1').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS','http://localhost:8081').split(',')
 
 # Application definition
 
